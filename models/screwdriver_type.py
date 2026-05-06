92bd7188ce962b72601d8f2d36e5412492914276
Vedran Saracevic <vedran.saracevic@merath.com>
2026-05-06 22:02:45 +0200

    Update screwdriver type model

--- models/screwdriver_type.py
+++ models/screwdriver_type.py
@@ -2,8 +2,11 @@ from odoo import models, fields
 
 
 class ScrewdriverType(models.Model):
-    _name = 'screwdriver.type'
-    _description = 'Screwdriver Type'
+    _name = "screwdriver.type"
+    _description = "Screwdriver Type"
 
-    name = fields.Char(string='Name', required=True)
-    description = fields.Text(string='Description')
+    # id is automatically created
+    name = fields.Char(string="Name", required=True)
+    name_old_sys = fields.Char(string="Name Old System")
+    description = fields.Text(string="Description")
+    active = fields.Boolean(string="Active", default=True)
