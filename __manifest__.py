d4252aaf5559860c384a13d2b1e7595960a92a25
Vedran Saracevic <vedran.saracevic@merath.com>
2026-05-06 20:54:51 +0200

    Make module installable and visible as an app

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Screwdriver Rent Management",
+    "version": "17.0.1.0",
+    "category": "Inventory",
+    "summary": "Manage screwdriver rentals and inventory",
+    "description": "A module to manage screwdriver rentals and inventory in Odoo",
+    "author": "Vedran S",
+    "depends": [
+        "base"
+    ],
+    "data": [
+        "views/screwdriver_menu.xml"
+    ],
+    "installable": True,
+    "application": True,
+    "license": "LGPL-3",
+}
